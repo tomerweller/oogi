64e1ef8dc3abfb32fec2db5fb67891a29cfe12ea
Nicholas DiPiazza <nicholas.dipiazza@gmail.com>
2018-06-23 01:18:08 -0500

    fix race condition

--- content.js
+++ content.js
@@ -1,10 +1,44 @@
-var headElement = (document.head||document.documentElement);
+var actualCode = `var cookieGetter = document.__lookupGetter__("cookie").bind(document);
+var cookieSetter = document.__lookupSetter__("cookie").bind(document);
 
-var injectJs = function(fileName) {
-    var s = document.createElement('script');
-    s.src = chrome.extension.getURL(fileName);
-    headElement.insertBefore(s, headElement.firstElementChild);
+var getPrefix = function() {
+    return "oogi$"
 };
 
-injectJs("common.js");
-injectJs("inject.js");
+var processCookieStr = function(cookiesStr) {
+    var prefix = getPrefix();
+    var cookieStrList = cookiesStr.split('; ');
+    var newStrList = [];
+    cookieStrList.forEach(function(cookieStr){
+        if (cookieStr.indexOf(prefix)==0) {
+            newStrList.push(cookieStr.substring(prefix.length, cookieStr.length));
+        }
+    });
+    return newStrList.join("; ");
+};
+
+var processSetCookieStr = function(str) {
+    console.log("Processing set cookie string " + str);
+    return getPrefix()+str;
+};
+
+Object.defineProperty(document, 'cookie', {
+    get: function() {
+        var storedCookieStr = cookieGetter();
+        console.log("Intercepted a cookie get " + storedCookieStr + " , and returning processed cookie string " + processCookieStr(storedCookieStr));
+        return processCookieStr(storedCookieStr);
+    },
+
+    set: function(cookieString) {
+        var newValue = processSetCookieStr(cookieString);
+        console.log("Intercepted a cookie set " + newValue)
+        return cookieSetter(newValue);
+    }
+});
+console.log("cookie get/set injector completed");
+`;
+
+var script = document.createElement('script');
+script.textContent = actualCode;
+(document.head||document.documentElement).appendChild(script);
+script.remove();
